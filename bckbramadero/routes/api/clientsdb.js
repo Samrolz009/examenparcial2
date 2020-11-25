6efd6ba1cbb3bd83be748928b3ae4f72c5a79306
Samuel <samuel368161@gmail.com>
2020-11-24 19:04:13 -0600

    resto

--- bckbramadero/routes/api/clientsdb.js
+++ bckbramadero/routes/api/clientsdb.js
@@ -26,6 +26,42 @@ router.post('/new', async (req, res)=>{
   }
 });
 
+router.get('/one/:id', async (req, res)=>{
+    try{
+      let { id } = req.params;
+      let oneDocument = await mdbClientModel.getById(id);
+      res.status(200).json(oneDocument);
+    } catch(ex){
+      console.log(ex);
+      res.status(500).json({ "msg": "Algo Paso Mal." });
+    }
+  });
+
+router.put('/upd/:id', async (req, res)=>{
+  try{
+    let {id} = req.params;
+    let {status} = req.body;
+    let rslt = await mdbClientModel.updateById(id,status);
+    res.status(200).json(rslt);
+  }catch(ex){
+    console.log(ex);
+    res.status(500).json({ "msg": "Algo Paso Mal." });
+  }
+});
+
+
+router.delete('/del/:id',async (req, res)=>{
+  let {id} = req.params;
+  try{
+    let rslt = await mdbClientModel.removeById(id);
+    res.status(200).json(rslt);
+  }catch(ex){
+    console.log(ex);
+    res.status(500).json({ "msg": "Algo Paso Mal." });
+  }
+  
+});
+
 
 
 module.exports = router;
